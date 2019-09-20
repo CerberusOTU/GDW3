71a2bcf594d40a5c916244eb38187b850273e764
Noah Lachine <noah.lachine@ontariotechu.net>
2019-09-19 20:12:09 -0400

    Reload and Update Muzzle Flash
    The ability to reload, the gun is not able to shoot when reloading. No animation while reloading. Updated muzzle flash (temporary), but easier to see reload mechanic. Adjust ammo count in the gun script.

--- Year3-Game/Assets/Scripts/Gun.cs
+++ Year3-Game/Assets/Scripts/Gun.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class Gun : MonoBehaviour
 {
@@ -7,14 +8,32 @@ public class Gun : MonoBehaviour
     public float impactForce = 30f;
     public float fireRate = 10f;
 
+    public int maxAmmo = 15;
+    private int currentAmmo = -1;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
 
     private float nextTimetoFire = 0f;
 
+    void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
+
     // Update is called once per frame
     void Update()
     {
+        if (isReloading)
+            return;
+
+        if (currentAmmo <=0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
         //player has inputted time to shoot
         if (Input.GetButton("Fire1") && Time.time >= nextTimetoFire)
         {
@@ -23,8 +42,20 @@ public class Gun : MonoBehaviour
         }
     }
 
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading...");
+
+        yield return new WaitForSeconds(reloadTime);
+
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
+
     void Shoot()
     {
+        currentAmmo--;
         muzzleFlash.Play();
         RaycastHit hitInfo;
 
