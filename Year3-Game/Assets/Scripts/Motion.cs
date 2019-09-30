61bfe69a2444e31fab6b3ba2461096e2dad8e512
Eashvar Balachandran <eashvar.balachandran@uoit.net>
2019-09-30 18:00:04 -0400

    Updated Movement & Look

---
+++ Year3-Game/Assets/Scripts/Motion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motion : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
+        float verticalMove = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontalMove, 0, verticalMove);
+        direction.Normalize();
+
+        rb.velocity = transform.TransformDirection(direction) * speed * Time.deltaTime;
+    }
+}
