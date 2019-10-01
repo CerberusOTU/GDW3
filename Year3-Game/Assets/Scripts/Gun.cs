5f04a31f9995f59fad2f7e64aff9693ef68f0371
Eashvar Balachandran <eashvar.balachandran@uoit.net>
2019-10-01 16:32:51 -0400

    Jumping Needs Fixing

--- Year3-Game/Assets/Scripts/Gun.cs
+++ Year3-Game/Assets/Scripts/Gun.cs
@@ -6,22 +6,15 @@ public class Gun : MonoBehaviour
 {
     public Text scoreText;
 
-    //Basic Gun Properties
     public float damage = 10f;
     public float range = 100f;
     public float impactForce = 30f;
     public float fireRate = 10f;
 
-    //Reload Properties
     public int maxAmmo = 30;
     private int currentAmmo = -1;
     public float reloadTime = 1f;
     private bool isReloading = false;
-
-    //Recoil Properties
-    public float maxRecoil_x = -20f;
-    public float recoilSpeed = 10f;
-    private float currentRecoil = 0f;
 
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
@@ -36,7 +29,6 @@ public class Gun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Determine whether reloading or out of ammo
         if (isReloading)
             return;
 
@@ -47,29 +39,20 @@ public class Gun : MonoBehaviour
             return;
         }
 
-
         else scoreText.text = currentAmmo.ToString();
 
         //player has inputted time to shoot
         if (Input.GetButton("Fire1") && Time.time >= nextTimetoFire)
         {
             nextTimetoFire = Time.time + 1f / fireRate;
-            
-            //Recoil
-            currentRecoil += 0.1f;
-
             Shoot();
         }
-        
 
-        //As long as clip is not full, player can reload
         if (Input.GetKey(KeyCode.R) && currentAmmo != maxAmmo)
         {
             scoreText.text = "Reloading";
             StartCoroutine(Reload());
         }
-
-        
     }
 
     IEnumerator Reload()
@@ -90,32 +73,28 @@ public class Gun : MonoBehaviour
         muzzleFlash.Play();
         RaycastHit hitInfo;
 
-        //Add recoil to move the camera and player orientation
-        if (currentRecoil > 0)
-        {
-            Quaternion maxRecoil = Quaternion.Euler (maxRecoil_x, 0f, 0f);
-            fpsCam.transform.localRotation = Quaternion.Slerp(fpsCam.transform.localRotation,maxRecoil,Time.deltaTime * recoilSpeed);
-            currentRecoil -= Time.deltaTime;
-        }
-        else
-        {
-            currentRecoil = 0f;
-            fpsCam.transform.localRotation = Quaternion.Slerp (fpsCam.transform.localRotation, Quaternion.identity, Time.deltaTime * recoilSpeed / 2);
-        }
-
         //check if we actually hit anything
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hitInfo, range))
         {
             Debug.Log(hitInfo.transform.name);
 
             //find the target component on obect we just hit and store it
-            Target target = hitInfo.transform.GetComponent<Target>();
+            //Target target = hitInfo.transform.GetComponent<Target>();
+
+            DestructionTarget desTarget = hitInfo.transform.GetComponent<DestructionTarget>();
 
             //apply damage to target
-            if (target != null)
+           // if (target != null)
+           // {
+           //     target.takeDamage(damage);
+            //}
+
+             //apply damage to target
+            if (desTarget != null)
             {
-                target.takeDamage(damage);
+                desTarget.takeDamage(damage);
             }
+
 
             //apply force to target on impact
             if (hitInfo.rigidbody != null)
--- Year3-Game/Assets/Scripts/Motion.cs
+++ Year3-Game/Assets/Scripts/Motion.cs
@@ -7,21 +7,50 @@ public class Motion : MonoBehaviour
     public float speed;
     public float sprintModifier;
     private Rigidbody rb;
+    private CapsuleCollider col;
+    private float distToGround;
 
-    // Start is called before the first frame update
+    public float jumpForce;
+    //public GameObject groundDetect;
+    public LayerMask ground;
+
+    //camera FOV when sprinting variables
+    public Camera cam;
+    private float baseFOV;
+    private float FOVmod = 0.75f;
+
     void Start()
     {
+        baseFOV = cam.fieldOfView;
         rb = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
+        distToGround = col.bounds.extents.y;
     }
+    //Check if player is grounded
+    bool isGrounded()
+    {
+       return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f, ground);
+    }
+
 
     // Update is called once per frame
     void FixedUpdate()
     {
         float horizontalMove = Input.GetAxisRaw("Horizontal");
         float verticalMove = Input.GetAxisRaw("Vertical");
-        
+
+        //bool isGrounded = Physics.Raycast(groundDetect.transform.localPosition, Vector3.down, 0.1f, ground);
+        //bool jump = Input.GetKey(KeyCode.Space);
+        //bool isJumping = jump && isGrounded;
+
         bool sprint = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
-        bool isSprinting = sprint && verticalMove > 0;
+        bool isSprinting = sprint && verticalMove > 0; //&& !isJumping;
+        
+
+        if (isGrounded() && Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        } 
 
         Vector3 direction = new Vector3(horizontalMove, 0, verticalMove);
         direction.Normalize();
@@ -31,8 +60,17 @@ public class Motion : MonoBehaviour
         if(isSprinting)
         {
             adjustedSpeed *= sprintModifier;
+            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, baseFOV * FOVmod, Time.deltaTime * 8f);
+        }
+        else
+        {
+            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, baseFOV, Time.deltaTime * 8f);
         }
 
-        rb.velocity = transform.TransformDirection(direction) * adjustedSpeed * Time.fixedDeltaTime;
+        Vector3 targetVelocity = transform.TransformDirection(direction) * adjustedSpeed * Time.fixedDeltaTime;
+        targetVelocity.y = rb.velocity.y;
+        rb.velocity =  targetVelocity;
+
+        
     }
 }
