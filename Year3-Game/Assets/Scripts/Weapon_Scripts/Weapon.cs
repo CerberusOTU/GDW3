71e399e7cfea2bca791addac1e6d0ae0b9f02157
Eashvar Balachandran <eashvar.balachandran@uoit.net>
2019-10-14 17:30:28 -0400

    Switch Code Fix

--- Year3-Game/Assets/Scripts/Weapon_Scripts/Weapon.cs
+++ Year3-Game/Assets/Scripts/Weapon_Scripts/Weapon.cs
@@ -75,7 +75,7 @@ public class Weapon : MonoBehaviour
 
         currentIndex = _ind;
 
-        GameObject newWeapon = Instantiate(loadout[_in  d].obj, weaponParent.position, weaponParent.rotation, weaponParent) as GameObject; 
+        GameObject newWeapon = Instantiate(loadout[_ind].obj, weaponParent.position, weaponParent.rotation, weaponParent) as GameObject; 
         newWeapon.transform.localPosition = Vector3.zero;   
         newWeapon.transform.localEulerAngles = Vector3.zero;   
 
