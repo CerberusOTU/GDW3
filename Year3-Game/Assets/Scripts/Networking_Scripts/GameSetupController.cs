1914e3e39dab1256af1d6e330848aba1f03703d9
Noah Lachine <noah.lachine@uoit.net>
2019-10-27 21:58:33 -0400

    Photon Networking Setup
    Able to user the networking scene to join a game based on server. This will instantiate an object.

---
+++ Year3-Game/Assets/Scripts/Networking_Scripts/GameSetupController.cs
@@ -0,0 +1,18 @@
+﻿using Photon.Pun;
+using System.IO;
+using UnityEngine;
+
+public class GameSetupController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       CreatePlayer(); //Create networked player object for each player. 
+    }
+
+    private void CreatePlayer()
+    {
+        Debug.Log("Creating Player");
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonPlayer"), Vector3.zero, Quaternion.identity);
+    }
+}
---
+++ Year3-Game/Assets/Scripts/Networking_Scripts/QuickStart/RoomControllerQuickStart.cs
@@ -0,0 +1,35 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomControllerQuickStart : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private int multiplayerSceneIndex; //Number for the build index to the multiplayer scene
+
+    public override void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    public override void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    public override void OnJoinedRoom() //Callback for when successfully join or create room
+    {
+        Debug.Log("Joined Room");
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log("Starting Game");
+            PhotonNetwork.LoadLevel(multiplayerSceneIndex); // Because of AutoSyncScene
+        }
+    }
+}
