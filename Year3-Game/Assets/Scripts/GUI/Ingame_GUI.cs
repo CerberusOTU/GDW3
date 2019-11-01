819ce8dd535afadf532a5d20aa14018423dc792a
Noah Lachine <noah.lachine@ontariotechu.net>
2019-11-01 17:17:02 -0400

    Working GUI for Trigger Events

---
+++ Year3-Game/Assets/Scripts/GUI/Ingame_GUI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ingame_GUI : MonoBehaviour
+{
+    
+    public int BoxFontSize;
+    public int LabelFontSize;
+    public int ButtonFontSize;
+    public string test;
+    public Texture2D temp;
+    public List<Texture2D> crossList;
+
+    void Start()
+    {
+        for(int i =0; i < 7; i++)
+        {
+            crossList.Add(temp);
+        }
+    }
+    
+    void OnGUI()
+    {
+        GUI.skin.label.fontSize = LabelFontSize;
+        GUI.skin.box.fontSize = BoxFontSize;
+        GUI.skin.button.fontSize = ButtonFontSize;
+
+        GUILayout.BeginArea (new Rect (Screen.width - 300, (Screen.height * 0.5f) - 350, 300, 350));
+        GUI.Box(new Rect(0,0,300,350), test);
+
+        GUI.Label(new Rect(10,50, 30, 30), crossList[0]);
+        GUI.Label(new Rect(50,55, 200, 30), "Move");
+
+        GUI.Label(new Rect(10,90, 30, 30), crossList[1]);
+        GUI.Label(new Rect(50,95, 200, 30), "Jump");
+
+        GUI.Label(new Rect(10,130, 30, 30), crossList[2]);
+        GUI.Label(new Rect(50,135, 200, 30), "Crouch");
+
+        GUI.Label(new Rect(10,170, 30, 30), crossList[3]);
+        GUI.Label(new Rect(50,175, 200, 30), "Pick Up / Swap");
+
+        GUI.Label(new Rect(10,210, 30, 30), crossList[4]);
+        GUI.Label(new Rect(50,215, 200, 30), "Shooting / Aim");
+
+        GUI.Label(new Rect(10,250, 30, 30), crossList[5]);
+        GUI.Label(new Rect(50,255, 200, 30), "Grenade");
+
+        GUI.Label(new Rect(10,290, 30, 30), crossList[6]);
+        GUI.Label(new Rect(50,295, 200, 30), "Find Partner");
+        GUILayout.EndArea();
+    }
+}
---
+++ Year3-Game/Assets/Scripts/GUI/Tutorial_Manager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial_Manager : MonoBehaviour
+{
+    private Ingame_GUI tutorialGUI;
+    // Game Achievement Triggers //
+    public GameObject MovementTrigger; //WASD
+    public GameObject JumpTrigger; //Space
+    public GameObject CrouchTrigger; //CTRL
+    public GameObject PickUpTrigger; //"E"
+    public GameObject ShootingTrigger; // LClick, RClick
+    public GameObject XRayTrigger; //"F"
+    public Texture2D isCompleteTex;
+    public Texture2D notCompleteTex;
+
+    // Player Components //
+    private Rigidbody playerRB;
+    private CapsuleCollider playerCC; 
+
+    void Start()
+    {
+        tutorialGUI = GameObject.FindObjectOfType<Ingame_GUI>();
+        playerRB = this.GetComponent<Rigidbody>();
+        playerCC = this.GetComponent<CapsuleCollider>();
+        for(int i =0; i < 7; i++)
+        {
+            tutorialGUI.crossList[i] = notCompleteTex;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //Check Player movement completion
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag != "isUnlocked")
+        {
+            if (collider.gameObject == MovementTrigger)
+            {
+                Debug.Log("Movement Complete");
+                Notify("MOVEMENT_COMPLETE");
+                tutorialGUI.crossList[0] = isCompleteTex;
+            }
+            else if(collider.gameObject == JumpTrigger)
+            {
+                Debug.Log("Jump Complete");
+                Notify("JUMP_COMPLETE");
+            }
+            else if(collider.gameObject == CrouchTrigger)
+            {       
+                Debug.Log("Crouch Complete");
+                Notify("CROUCH_COMPLETE");  
+            }
+            collider.gameObject.tag = "isUnlocked";
+        }
+    }
+
+    void Notify(string _achievement)
+    {
+        switch (_achievement)
+        {
+            case "MOVEMENT_COMPLETE":
+                tutorialGUI.crossList[0] = isCompleteTex;
+            break;
+            case "JUMP_COMPLETE":
+                tutorialGUI.crossList[1] = isCompleteTex;
+            break;
+            case "CROUCH_COMPLETE":
+                tutorialGUI.crossList[2] = isCompleteTex;
+            break;
+        }
+    }
+}
