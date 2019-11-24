d68de01819709033bde8c3d898cb047e26fc796b
Adam Bilyea <adam.bilyea@uoit.net>
2019-11-24 13:59:47 -0500

    Added Menus and loading Scenes

---
+++ Year3-Game/Assets/Scripts/Menu/Loading.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Loading : MonoBehaviour
+{
+    [SerializeField]
+    private Image progressBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //start async 
+        StartCoroutine(LoadOperation());
+    }
+
+    IEnumerator LoadOperation()
+    {
+        AsyncOperation gameLevel = SceneManager.LoadSceneAsync("SampleScene");
+        
+        while (gameLevel.progress < 1)
+        {
+            progressBar.fillAmount = gameLevel.progress;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
---
+++ Year3-Game/Assets/Scripts/Menu/MenuControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuControl : MonoBehaviour
+{
+
+    public Animator transitionsAnim;
+    public string sceneName;
+
+    // Update is called once per frame
+    public void PlayGame()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    IEnumerator LoadScene()
+    {
+        transitionsAnim.SetTrigger("end");
+        yield return new WaitForSeconds(0.7f);
+        SceneManager.LoadScene(sceneName);
+
+
+    }
+}
