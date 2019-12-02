9527b462f4c92470ba43f6aec514c58e83b29e70
Adam Bilyea <adam.bilyea@ontariotechu.net>
2019-12-02 16:41:36 -0500

    Fixed Grenade issue, added 2 more guns

---
+++ Year3-Game/Assets/Scripts/QuickCountdown.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickCountdown : MonoBehaviour
+{
+
+    float timer;
+
+    void Start()
+    {
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer < 0.1f)
+        {
+            timer += Time.deltaTime;
+            GameObject.Find("Player").GetComponent<Weapon>().enabled = false;
+            GameObject.Find("Player2").GetComponent<Weapon2>().enabled = false;
+        }
+        else
+        {
+            GameObject.Find("Player").GetComponent<Weapon>().enabled = true;
+            GameObject.Find("Player2").GetComponent<Weapon2>().enabled = true;
+            Destroy(this);
+        }
+    }
+}
