fce934fd95918f5a26ca5a70beaebb8c44f3b675
Adam Bilyea <adam.bilyea@uoit.net>
2020-01-26 23:12:09 -0500

    Added Scene

---
+++ Year3-Game/Assets/_Scripts/Menu_Scripts/MeshSwitcher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshSwitcher : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y > -12)
+        {
+            float step = speed * Time.deltaTime;
+
+            transform.position = new Vector3(transform.position.x, transform.position.y - 3, transform.position.z);    
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y < 0)
+        {
+            float step = speed * Time.deltaTime;
+
+            transform.position = new Vector3(transform.position.x, transform.position.y + 3, transform.position.z);    
+        }
+    }
+}
