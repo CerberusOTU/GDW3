97bde95432cb231fffbafebb9fb8482a2901bc54
Noah Lachine <noah.lachine@ontariotechu.net>
2020-02-06 17:40:33 -0500

    [REFACTOR/ADD] Look script, player prefab

---
+++ Year3-Game/Assets/_Scripts/__TESTING(NOAH)/playerCharController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCharController : MonoBehaviour
+{
+
+    CharacterController characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        GroundCheck();
+    }
+
+    public void GroundCheck()
+    {
+        //float distanceToGround = 0f;
+
+        //if(Physics.CapsuleCast(capsulePoint1(), capsulePoint2(), characterController.radius, -transform.up, Mathf.Infinity, , QueryTriggerInteraction.Ignore))
+        //{
+
+        //}
+
+    }
+
+    //Point 1 - Center of sphere at the START of the capsule
+    Vector3 capsulePoint1()
+    {
+        return transform.position + characterController.center + Vector3.up * -characterController.height * 0.5F;
+    }
+    //Point 2 - Center of sphere at the START of the capsule
+    Vector3 capsulePoint2()
+    {
+        return capsulePoint1() + Vector3.up * characterController.height;
+    }
+}
---
+++ Year3-Game/Assets/_Scripts/__TESTING(NOAH)/playerLook.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerLook : MonoBehaviour
+{
+
+    [Header("Mouse Options")]
+    [Range(1.0f, 15.0f)] [SerializeField] private float mouseSensitivity = 5.0f;
+    [SerializeField] private bool InvertYAxis = false;
+
+    /*=====Private Variables=====*/
+    //Float
+    private float xRotation = 0f;
+    //Camera
+    private Camera fpsCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fpsCamera = GetComponentInChildren<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseMovement();
+    }
+
+    void mouseMovement()
+    {
+        float mouseX = Input.GetAxisRaw(inputConstants.c_MouseAxisNameHorizontal) * mouseSensitivity * 10f * Time.deltaTime;
+        float mouseY = Input.GetAxisRaw(inputConstants.c_MouseAxisNameVertical) * mouseSensitivity * 10f * Time.deltaTime;
+
+        if (InvertYAxis)
+        {
+            xRotation += mouseY;
+        }
+        else
+        {
+            xRotation -= mouseY;
+        }
+
+        xRotation = Mathf.Clamp(xRotation, -89f, 89f);
+
+        fpsCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
