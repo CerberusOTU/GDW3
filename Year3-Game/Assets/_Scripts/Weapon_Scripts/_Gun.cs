1f098271f3a2a4261eb395abf209e674d3c50fd1
Noah Lachine <noah.lachine@ontariotechu.net>
2020-02-16 16:38:23 -0500

    [MOD] Added weapon ID number to _gun scriptable OBJ

--- Year3-Game/Assets/_Scripts/Weapon_Scripts/_Gun.cs
+++ Year3-Game/Assets/_Scripts/Weapon_Scripts/_Gun.cs
@@ -5,35 +5,38 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Gun", menuName = "Gun")]
 public class _Gun : ScriptableObject
 {
-   public string name;
-   public string className;
-   public string ShotType;
-   public float damage;
-   public float firerate;
-   public float bloom;
-   public float recoil;
-   public float maxRecoil_x;
-   public float recoilSpeed;
-   public float recoilDampen; //Less is more
-   public float kickBack;
-   public float aimSpeed;
+    public int weaponID;
+    //0   M1911
+    //1   TommyGun
+    //2   MP40
+    //3   Shotgun
+    //4   Revolver
 
-   //Ammo & reloading
-   public float reloadTime;
+    public string name;
+    public string className;
+    public string ShotType;
+    public float damage;
+    public float firerate;
+    public float bloom;
+    public float recoil;
+    public float maxRecoil_x;
+    public float recoilSpeed;
+    public float recoilDampen; //Less is more
+    public float kickBack;
+    public float aimSpeed;
 
-   public int alwaysMax;
-   public int maxAmmo;
-   public int clipSize;
-
-   public int currentAmmo;
+    //Ammo & reloading
+    public float reloadTime;
+    public int alwaysMax;
+    public int maxAmmo;
+    public int clipSize;
+    public int currentAmmo;
+    public bool isReloading;
 
     //Sound
     public string ShotPath;
     public string ReloadPath;
 
-   [SerializeField]
-   public bool isReloading;
-   //////////////
-   public GameObject weaponObj;
-   public GameObject weaponObj_Arms;
+    public GameObject weaponObj;
+    public GameObject weaponObj_Arms;
 }
