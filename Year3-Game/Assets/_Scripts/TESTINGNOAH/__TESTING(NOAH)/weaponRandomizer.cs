bcec737e23614dd99d67da609d2a4a031d98fb9a
Eashvar Balachandran <eashvar.balachandran@uoit.net>
2020-03-10 13:44:22 -0400

    Movement&Look

---
+++ Year3-Game/Assets/_Scripts/TESTINGNOAH/__TESTING(NOAH)/weaponRandomizer.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponRandomizer : MonoBehaviour
+{
+    [Header("Prefabs")]
+    [SerializeField] private GameObject tommyGun;
+    [SerializeField] private GameObject mp40;
+    [SerializeField] private GameObject revolver;
+    [SerializeField] private GameObject shotgun;
+
+    [Header("Gun Spawn Points")]
+    [SerializeField] private Transform[] spawnlocations;
+    public List<GameObject> sceneWeapons;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnlocations = GetComponentsInChildren<Transform>();
+        spawnGuns();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            despawnGuns();
+        }
+    }
+
+    public void spawnGuns()
+    {
+        for (int index = 0; index < spawnlocations.Length; index++)
+        {
+
+            if (spawnlocations[index].gameObject.tag == "Spawn_TommyGun")
+            {
+                GameObject temp = Instantiate(tommyGun, spawnlocations[index].position, spawnlocations[index].localRotation);
+                sceneWeapons.Add(temp);
+            }
+            else if (spawnlocations[index].gameObject.tag == "Spawn_MP40")
+            {
+                GameObject temp = Instantiate(mp40, spawnlocations[index].position, spawnlocations[index].localRotation);
+                sceneWeapons.Add(temp);
+            }
+            else if (spawnlocations[index].gameObject.tag == "Spawn_Shotgun")
+            {
+                GameObject temp = Instantiate(shotgun, spawnlocations[index].position, spawnlocations[index].localRotation);
+                sceneWeapons.Add(temp);
+            }
+            else if (spawnlocations[index].gameObject.tag == "Spawn_Revolver")
+            {
+                GameObject temp = Instantiate(revolver, spawnlocations[index].position, spawnlocations[index].localRotation);
+                sceneWeapons.Add(temp);
+            }
+            else
+            {
+                int randGun = Random.Range(0, 100); //Random 0-100
+                if (randGun >= 0 && randGun < 20) //20%
+                {
+                    //Instantiate temp obj tommy
+                    GameObject temp = Instantiate(tommyGun, spawnlocations[index].position, spawnlocations[index].localRotation);
+                    sceneWeapons.Add(temp);
+                }
+                else if (randGun >= 20 && randGun < 50) //30%
+                {
+                    //Instantiate temp obj mp40
+                    GameObject temp = Instantiate(mp40, spawnlocations[index].position, spawnlocations[index].localRotation);
+                    sceneWeapons.Add(temp);
+                }
+                else if (randGun >= 50 && randGun < 70) //20%
+                {
+                    //Instantiate temp obj shotgun
+                    GameObject temp = Instantiate(shotgun, spawnlocations[index].position, spawnlocations[index].localRotation);
+                    sceneWeapons.Add(temp);
+                }
+                else if (randGun >= 70 && randGun <= 100) //30%
+                {
+                    //Instantiate temp obj revolver
+                    GameObject temp = Instantiate(revolver, spawnlocations[index].position, spawnlocations[index].localRotation);
+                    sceneWeapons.Add(temp);
+                }
+            }
+        }
+    }
+
+    public void despawnGuns()
+    {
+        for (int index = 0; index < sceneWeapons.Count; index++)
+        {
+            Destroy(sceneWeapons[index]);
+        }
+    }
+}
--- Year3-Game/Assets/_Scripts/Weapon_Scripts/_Gun.cs
+++ Year3-Game/Assets/_Scripts/Weapon_Scripts/_Gun.cs
@@ -5,34 +5,42 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Gun", menuName = "Gun")]
 public class _Gun : ScriptableObject
 {
-   public string name;
-   public string className;
-   public string ShotType;
-   public float damage;
-   public float firerate;
-   public float bloom;
-   public float recoil;
-   public float maxRecoil_x;
-   public float recoilSpeed;
-   public float recoilDampen; //Less is more
-   public float kickBack;
-   public float aimSpeed;
-
-   //Ammo & reloading
-   public float reloadTime;
-
-   public int alwaysMax;
-   public int maxAmmo;
-   public int clipSize;
-
+   //REMOVE LATER
    public int currentAmmo;
+   public int maxAmmo;
+   public GameObject obj;
+
+   /////////////////////////////////////
+  public int weaponID;
+    //0   M1911
+    //1   TommyGun
+    //2   MP40
+    //3   Shotgun
+    //4   Revolver
+
+    public string name;
+    public string className;
+    public string ShotType;
+    public float damage;
+    public float firerate;
+    public float bloom;
+    public float recoil;
+    public float maxRecoil_x;
+    public float recoilSpeed;
+    public float recoilDampen; //Less is more
+    public float kickBack;
+    public float aimSpeed;
+
+    //Ammo & reloading
+    public float reloadTime;
+    public int alwaysMax;
+    public int clipSize;
+    public bool isReloading;
 
     //Sound
     public string ShotPath;
     public string ReloadPath;
 
-   [SerializeField]
-   public bool isReloading;
-   //////////////
-   public GameObject obj;
+    public GameObject weaponObj;
+    public GameObject weaponObj_Arms;
 }
