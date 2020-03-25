0bf473cae1503f7b3da71d20cd47f368d2c3b87f
Ricky <teo.cacereshernandez@uoit.net>
2020-03-25 15:10:25 -0400

    Menu work
    New menu initialized with all components, needs assets

--- Year3-Game/Assets/_Scripts/Menu_Scripts/MenuControl.cs
+++ Year3-Game/Assets/_Scripts/Menu_Scripts/MenuControl.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
-
+using UnityEngine.Playables;
 public class MenuControl : MonoBehaviour
 {
 
     public Animator transitionsAnim;
     public string sceneName;
+    public PlayableDirector Timeline;
+    public PlayableDirector TimelineBack;
+
 
     void Start()
     {    
@@ -21,6 +24,16 @@ public class MenuControl : MonoBehaviour
     public void PlayGame()
     {
         StartCoroutine(LoadScene());
+    }
+
+    public void PlayAnimation()
+    {
+        Timeline.Play();
+    }
+
+    public void PlayBackAnimation()
+    {
+        TimelineBack.Play();
     }
 
     public void QuitGame()
