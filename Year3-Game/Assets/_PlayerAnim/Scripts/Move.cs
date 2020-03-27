02428f95bdc6fe450fa61c65d59c30474a8b491c
medhat <medhat.fayed@ontariotechu.net>
2020-03-27 18:21:28 -0400

    Player 2 animations

--- Year3-Game/Assets/_PlayerAnim/Scripts/Move.cs
+++ Year3-Game/Assets/_PlayerAnim/Scripts/Move.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
- private Animator animator;
+    private Animator animator;
     private Weapon weapon;
     public GameObject M1911;
     public GameObject Tommy;
@@ -26,7 +26,18 @@ public class Move : MonoBehaviour
 
         if (weapon.currentIndex == 1)
         {
-            animator.SetInteger("Switch", 1);
+            if(Input.GetKey(KeyCode.LeftShift))
+                {
+                    animator.SetInteger("Switch", 11);
+                    animator.SetBool("isSprinting", true);
+                    //transform.Rotate(new Vector3(0f,-20f,0f)); 
+                }
+                else
+                {
+                animator.SetInteger("Switch", 1);
+                animator.SetBool("isSprinting", false);
+                }
+            
             M1911.SetActive(true);
             Tommy.SetActive(false);
             Revolver.SetActive(false);
@@ -37,7 +48,17 @@ public class Move : MonoBehaviour
         {
             if(weapon.loadout[0].name == "Tommy")
             {
-            animator.SetInteger("Switch", 2);
+                if(Input.GetKey(KeyCode.LeftShift))
+                {
+                    animator.SetInteger("Switch", 22);
+                    animator.SetBool("isSprinting", true);
+                }
+                else
+                {
+                animator.SetInteger("Switch", 2);
+                animator.SetBool("isSprinting", false);
+                }
+
             M1911.SetActive(false);
             Tommy.SetActive(true);
             Revolver.SetActive(false);
@@ -48,7 +69,17 @@ public class Move : MonoBehaviour
         
             if(weapon.loadout[0].name == "Revolver")
             {
-            animator.SetInteger("Switch", 3);
+                if(Input.GetKey(KeyCode.LeftShift))
+                {
+                    animator.SetInteger("Switch", 33);
+                    animator.SetBool("isSprinting", true);
+                }
+                else
+                {
+                animator.SetInteger("Switch", 3);
+                animator.SetBool("isSprinting", false);
+                }
+            
             M1911.SetActive(false);
             Tommy.SetActive(false);
             Revolver.SetActive(true);
@@ -58,7 +89,17 @@ public class Move : MonoBehaviour
 
              if(weapon.loadout[0].name == "MP40")
             {
-            animator.SetInteger("Switch", 4);
+                 if(Input.GetKey(KeyCode.LeftShift))
+                {
+                    animator.SetInteger("Switch", 44);
+                    animator.SetBool("isSprinting", true);
+                }
+                else
+                {
+                animator.SetInteger("Switch", 4);
+                animator.SetBool("isSprinting", false);
+                }
+
             M1911.SetActive(false);
             Tommy.SetActive(false);
             Revolver.SetActive(false);
@@ -68,13 +109,25 @@ public class Move : MonoBehaviour
 
              if(weapon.loadout[0].name == "Shotgun")
             {
-            animator.SetInteger("Switch", 5);
+                if(Input.GetKey(KeyCode.LeftShift))
+                {
+                    animator.SetInteger("Switch", 55);
+                    animator.SetBool("isSprinting", true);
+                }
+                else
+                {
+                animator.SetInteger("Switch", 5);
+                animator.SetBool("isSprinting", false);
+                }
+                
             M1911.SetActive(false);
             Tommy.SetActive(false);
             Revolver.SetActive(false);
             MP40.SetActive(false);
             Shotgun.SetActive(true);
             }
+
+
         }
 
         var x = Input.GetAxis("Horizontal");
