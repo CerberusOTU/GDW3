4ab67244aabfd305da34b856851b4a37abbbd92c
Adam Bilyea <adam.bilyea@uoit.net>
2020-04-03 11:03:42 -0400

    Cinematic is in without audio

---
+++ Year3-Game/Assets/_Scenes/Menu Scenes/movie.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class movie : MonoBehaviour {
+	
+    public string sceneName;
+    float timer = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play ();
+
+
+	}
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        Debug.Log("MOVIE TIME = " + timer);
+
+        if (timer > 35)
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
